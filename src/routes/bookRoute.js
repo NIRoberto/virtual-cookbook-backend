44dc5da791cf995a295c78503c19d145543541ec
Robert Niyitanga <niroberto>
2024-01-22 10:10:31 +0200

    backend implementation

---
+++ src/routes/bookRoute.js
@@ -0,0 +1,11 @@
+// routes/bookingRoutes.js
+import express from "express";
+import bookingController from "../controllers/bookController";
+
+const router = express.Router();
+
+router.post("/:userId", bookingController.bookRecipe);
+router.get("/user/:userId", bookingController.getBookingsByUser);
+router.get("/recipe/:recipeId", bookingController.getBookingsByRecipe);
+
+export default router;
